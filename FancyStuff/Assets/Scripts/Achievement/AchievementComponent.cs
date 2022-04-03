9a71af9ec126dc78f8931b169a36db7bffb01bcd
Helen <helen-bohleber@web.de>
2022-04-03 11:55:08 +0200

    Add sample achievement without calling it

---
+++ FancyStuff/Assets/Scripts/Achievement/AchievementComponent.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Achievement
+{
+    public class AchievementComponent
+    {
+        public string Title;
+        public string Text;
+        public int ImageValue;
+        public bool Achieved;
+    }
+}
