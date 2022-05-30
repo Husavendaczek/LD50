71006a95996d3eb368b087778a687dd02a8e8542
HelenBohleber <helen.bohleber@brickmakers.de>
2022-05-30 22:19:02 +0200

    Add exit interactable

---
+++ FancyStuff/Assets/Scripts/Interaction/Exit.cs
@@ -0,0 +1,32 @@
+﻿using Inventory;
+using Messaging;
+using RoomScene;
+using UnityEngine;
+
+namespace Interaction
+{
+    public class Exit : MonoBehaviour, IInteractable
+    {
+        private IMediator _mediator;
+        public void ShowContextMenu()
+        {
+            _mediator.ShowSimpleMessage(new SimpleMessage {MessageText = "End the game"});
+        }
+
+        public void ShowText()
+        {
+            _mediator.ShowSimpleMessage(new SimpleMessage {MessageText = "Grow up"});
+        }
+
+        public void StartInteraction()
+        {
+            _mediator.ShowSimpleMessage(new SimpleMessage {MessageText = "You should grow up"});
+            _mediator.InteractionManagerHasSelectedItem(this);
+        }
+
+        public void Interact(InventoryItem item)
+        {
+            _mediator.ShowEnd();
+        }
+    }
+}
